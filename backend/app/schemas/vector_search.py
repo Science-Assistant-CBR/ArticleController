0ab5c3c78d52bd229344f6183267e354605e5dc4
Mikhail Voronin <voroninmms@gmail.com>
2025-03-23 01:43:01 +0300

    Added raw_return attribute to vector_search schema

--- backend/app/schemas/vector_search.py
+++ backend/app/schemas/vector_search.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 class VectorSearch(BaseModel):
     article_type: bool = Field(..., description="True - научные статьи, False - новости")
+    raw_return: bool = Field(False, description="Возвращаем сырые тексты points "
+                                                "или готовый сформулированный ответ от OpenAI")
     query_text: str = Field(..., description="Текст запроса")
     top_k: int = Field(5, gt=0, description="Количество релевантных points")
     source_name: Optional[str] = Field(..., description="Источник")
