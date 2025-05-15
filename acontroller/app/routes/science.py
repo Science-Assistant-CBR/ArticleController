c02b95df33a3f0d36b237f1eedd0eeb868aed814
Mikhail Voronin <voroninmms@gmail.com>
2025-05-15 21:32:43 +0300

    sphere field added to science_article model

--- acontroller/app/routes/science.py
+++ acontroller/app/routes/science.py
@@ -22,6 +22,8 @@ async def get_articles(
     if filters.title:
         stmt = stmt.where(ModelsScienceArticle.title.ilike(f"%{filters.title}%"))
     if filters.source_name:
+        stmt = stmt.where(ModelsScienceArticle.sphere == filters.sphere)
+    if filters.source_name:
         stmt = stmt.where(ModelsScienceArticle.source_name == filters.source_name)
     if filters.start_date:
         stmt = stmt.where(ModelsScienceArticle.published_date >= filters.start_date)
