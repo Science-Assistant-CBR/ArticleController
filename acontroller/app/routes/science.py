67c0eb9186d51d883b950d4792acb898c1111d60
Pavel Shakhmin <43382033+pshakhmin@users.noreply.github.com>
2025-05-26 19:08:17 +0300

    Update science.py

--- acontroller/app/routes/science.py
+++ acontroller/app/routes/science.py
@@ -15,7 +15,7 @@ router = APIRouter(prefix="/science", tags=["science"])
 
 @router.get("/articles", response_model=List[SchemasScienceArticle])
 async def get_articles(
-    filters: ScienceArticleFilter = Depends(),
+    filters: Annotated[ScienceArticleFilter, Query()],
     db: AsyncSession = Depends(get_db)
 ):
     stmt = select(ModelsScienceArticle)
